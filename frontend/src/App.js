d47961a9570900820d01154fd2293f5093467bbc
Aryan Chavan <aryan@aryansdomain.com>
2022-03-31 23:52:57 +0530

    add react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
 import { MantineProvider, ScrollArea } from "@mantine/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { MyNavbar } from "./components/Navbar";
 import config from "./config";
 
 function App() {
 	return (
-		<div className="App">
-			<MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles>
-				<MyNavbar links={config.navLinks} />
-				<ScrollArea className="main">main</ScrollArea>
-			</MantineProvider>
-		</div>
+		<BrowserRouter>
+			<div className="App">
+				<MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles>
+					<MyNavbar links={config.navLinks} />
+
+					<ScrollArea className="main">
+						<Routes>
+							<Route path="/" element={<h1>home</h1>} />
+							<Route path="*" element={<h1>404</h1>} />
+						</Routes>
+					</ScrollArea>
+				</MantineProvider>
+			</div>
+		</BrowserRouter>
 	);
 }
 
